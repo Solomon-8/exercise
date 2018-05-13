5c4bd38d5421689ddcd785a187ff608265da15d6
Solomon-8 <914573285@qq.com>
2018-05-13 15:45:37 +0800

    为了time_picker做调试准备

--- lib/pages/AddActivityPage.dart
+++ lib/pages/AddActivityPage.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddActivityPage extends StatefulWidget{
     @override
@@ -11,10 +13,44 @@ class AddActivityPage extends StatefulWidget{
 
 
 class AddActivityWidget extends State<AddActivityPage>{
+    final GlobalKey<FormState> _addActivityForm = new GlobalKey<FormState>();
+    String name;
+    DateTime dateTime;
+    String place;
+    int peopleNum;
+    DateTime selectedDate = new DateTime.now();
+    TimeOfDay selectedTime = const TimeOfDay(hour: 7, minute: 28);
+
     @override
     Widget build(BuildContext context) {
-        // TODO: implement build
-        return null;
+        return new Scaffold(
+            body: new Container(
+                    height: 120.0,
+                child: new Column(
+                    children: <Widget>[
+                        new Form(child: new Column(
+                            key: _addActivityForm,
+                            children: <Widget>[
+                                new TextFormField(
+                                    decoration: const InputDecoration(
+                                        icon: const Text("活动名称"),
+                                        border: const UnderlineInputBorder(),
+                                        hintText: '自定义'
+                                    ),
+                                    onSaved: (String value){name = value;},
+                                    keyboardType: TextInputType.text,
+                                ),
+
+                            ],
+                        ))
+                    ],
+                )
+            ),
+        );
     }
 
 }
+
+
+
+
